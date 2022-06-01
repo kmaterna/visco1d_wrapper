c8f3ca6e25fe41391a83c44a5fdc7e4902b62fcb
Brendan Meade <brendanjmeade@users.noreply.github.com>
2022-06-01 14:48:37 -0400

    Processing and visualization of mixed Maxwell and Burgers layer cases

--- run_visco1d.py
+++ run_visco1d.py
@@ -7,14 +7,41 @@ import uuid
 import pandas as pd
 
 # TODO: Replace os.system with subprocess?
+
+# Unit conversion constants
+KM_TO_M = 1e3
+CGS_DENSITY_TO_MKS_DENSITY = 1e3
 VISCO1D_BULK_MODULUS_TO_MKS_BULK_MODULUS = 1e10
 VISCO1D_SHEAR_MODULUS_TO_MKS_SHEAR_MODULUS = 1e10
 VISCO1D_VISCOSITY_TO_MKS_VISCOSITY = 1e18
+
+# Figure constants
 DPI_PNG = 500
 
 
 def read_earth_model(earth_model_file_name):
-    # Read earth model
+    """
+    Read earth model layers
+
+    Maxwell rheology columns:
+    1. bottom_radius
+    2. top_radius
+    3. density
+    4. bulk_modulus
+    5. maxwell_shear_modulus
+    6. maxwell_viscosity
+
+    Kelvin rheology columns:
+    1. bottom_radius
+    2. top_radius
+    3. density
+    4. bulk_modulus
+    5. maxwell_shear_modulus
+    6. kelvin_shear_modulus
+    7. maxwell_viscosity
+    8. kelvin_viscosity
+    """
+
     earth_model = {}
     earth_model["file_name"] = earth_model_file_name
 
@@ -25,14 +52,6 @@ def read_earth_model(earth_model_file_name):
         earth_model["radius"] = float(first_line_values[2])
         earth_model["depfac"] = float(first_line_values[3])
 
-    # Read earth.model Layers
-    # Maxwell only case columns:
-    # bottom_radius",
-    #         "top_radius",
-    #         "density",
-    #         "bulk_modulus",
-    #         "shear_modulus",
-    #         "maxwell_viscosity",
     earth_model["data"] = pd.read_csv(
         earth_model["file_name"],
         delim_whitespace=True,
@@ -46,29 +65,56 @@ def read_earth_model(earth_model_file_name):
             "to_process_1",  # Could be Maxwell viscosity or Kelvin shear modulus
             "to_process_2",  # First column present if Burgers layer
             "to_process_3",  # First column present if Burgers layer
-            "to_process_4",  # First column present if Burgers layer
         ],
     )
 
-    # Are there any Burgers rheology layers
-    any_burgers_layers = ~earth_model["data"]["to_process_2"].isnull().values.any()
+    # Process layers with Burgers rheology
+    earth_model["data"]["maxwell_viscosity"] = 0.0
+    earth_model["data"]["kelvin_shear_modulus"] = 0.0
+    earth_model["data"]["kelvin_viscosity"] = 0.0
+    any_burgers_layers = ~earth_model["data"]["to_process_2"].isnull().values.all()
     if any_burgers_layers:
-        pass
+        print("Found Burgers layers")
+        for i in range(len(earth_model["data"])):
+            if np.isnan(earth_model["data"]["to_process_2"][i]):
+                earth_model["data"]["maxwell_viscosity"].values[i] = earth_model[
+                    "data"
+                ]["to_process_1"].values[i]
+                earth_model["data"]["kelvin_shear_modulus"].values[i] = np.nan
+                earth_model["data"]["kelvin_viscosity"].values[i] = 0
+            else:
+                earth_model["data"]["kelvin_shear_modulus"].values[i] = earth_model[
+                    "data"
+                ]["to_process_1"].values[i]
+                earth_model["data"]["maxwell_viscosity"].values[i] = earth_model[
+                    "data"
+                ]["to_process_2"].values[i]
+                earth_model["data"]["kelvin_viscosity"].values[i] = earth_model["data"][
+                    "to_process_3"
+                ].values[i]
     else:
+        print("Found only Maxwell layers")
         earth_model["data"]["maxwell_viscosity"] = earth_model["data"]["to_process_1"]
+        earth_model["data"]["kelvin_shear_modulus"] = np.nan
+        earth_model["data"]["kelvin_viscosity"] = np.nan
 
-    # Physical and useful derived parameters
-    # bottom_radius: km
-    # top_radius: km
-    # density: g / cm**3
-    # bulk modulus: 10**10 Pa
-    # shear modulus:10**10 Pa
-    # viscosity: 10**18 Pa s
+    # Convert visco1d dimensions to MKS
+    earth_model["data"]["top_radius_mks"] = earth_model["data"]["top_radius"] * KM_TO_M
+    earth_model["data"]["bottom_radius_mks"] = (
+        earth_model["data"]["bottom_radius"] * KM_TO_M
+    )
+    earth_model["data"]["density_mks"] = (
+        earth_model["data"]["density"] * CGS_DENSITY_TO_MKS_DENSITY
+    )
     earth_model["data"]["bulk_modulus_mks"] = (
         earth_model["data"]["shear_modulus"] * VISCO1D_BULK_MODULUS_TO_MKS_BULK_MODULUS
     )
     earth_model["data"]["shear_modulus_mks"] = (
         earth_model["data"]["shear_modulus"]
+        * VISCO1D_SHEAR_MODULUS_TO_MKS_SHEAR_MODULUS
+    )
+    earth_model["data"]["kelvin_shear_modulus_mks"] = (
+        earth_model["data"]["kelvin_shear_modulus"]
         * VISCO1D_SHEAR_MODULUS_TO_MKS_SHEAR_MODULUS
     )
     earth_model["data"]["maxwell_viscosity_mks"] = (
@@ -77,6 +123,13 @@ def read_earth_model(earth_model_file_name):
     earth_model["data"]["maxwell_viscosity_mks_log10"] = np.log10(
         earth_model["data"]["maxwell_viscosity_mks"]
     )
+    earth_model["data"]["kelvin_viscosity_mks"] = (
+        earth_model["data"]["kelvin_viscosity"] * VISCO1D_VISCOSITY_TO_MKS_VISCOSITY
+    )
+    earth_model["data"]["kelvin_viscosity_mks_log10"] = np.log10(
+        earth_model["data"]["kelvin_viscosity_mks"]
+    )
+
     return earth_model
 
 
@@ -103,21 +156,31 @@ def plot_earth_model(earth_model, output_folder_name):
         plt.xlabel(x_label_text)
         plt.ylabel("radius (km)")
 
-    plt.figure(figsize=(15, 5))
-    plt.subplot(1, 4, 1)
-    plot_radius_subplot(earth_model, "density", "density (g/cm^3)")
+    plt.figure(figsize=(20, 5))
+    plt.subplot(1, 6, 1)
+    plot_radius_subplot(earth_model, "density_mks", "density (kg/m^3)")
 
-    plt.subplot(1, 4, 2)
+    plt.subplot(1, 6, 2)
     plot_radius_subplot(earth_model, "bulk_modulus_mks", "bulk modulus (Pa)")
 
-    plt.subplot(1, 4, 3)
+    plt.subplot(1, 6, 3)
     plot_radius_subplot(earth_model, "shear_modulus_mks", "shear modulus (Pa)")
 
-    plt.subplot(1, 4, 4)
+    plt.subplot(1, 6, 4)
     plot_radius_subplot(
         earth_model,
         "maxwell_viscosity_mks_log10",
         "log10 Maxwell viscosity (Pa s)",
+    )
+
+    plt.subplot(1, 6, 5)
+    plot_radius_subplot(earth_model, "kelvin_shear_modulus_mks", "shear modulus (Pa)")
+
+    plt.subplot(1, 6, 6)
+    plot_radius_subplot(
+        earth_model,
+        "kelvin_viscosity_mks_log10",
+        "log10 Kelvin viscosity (Pa s)",
     )
 
     plt.savefig(os.path.join(output_folder_name, "earth_model.png"), dpi=DPI_PNG)
@@ -136,10 +199,13 @@ def main():
         os.makedirs(output_folder_name)
 
     earth_model_file_name = "./data/earth.modelMAXWELL"
+    # earth_model_file_name = "./data/earth.modelBURG30"
+    print(earth_model_file_name)
+
     earth_model = read_earth_model(earth_model_file_name)
     plot_earth_model(earth_model, output_folder_name)
 
-    print(earth_model["data"])
+    # print(earth_model["data"])
 
     # success = os.system(f"cp {earth_model['file_name']} earth.model")
     # cp earth.modelHOMO30 earth.model
